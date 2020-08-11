751aaaea3fb304fbdef8510cbbd5fb6d83b2373e
Tiffany Valentine <52210944+devWitch-ctrl@users.noreply.github.com>
2020-08-11 16:03:48 -0600

    Second Commit
    Further Progress: nextSequence() {
    JQuery Effects (Fade In/Out, new Audio)
    };

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 const buttonColors = ["red", "blue", "green", "yellow"];
 
 
@@ -6,9 +5,20 @@ let gamePattern = [];
 
 
 function nextSequence() {
+
   let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColor = buttonColors[Math.floor(Math.random() * buttonColors.length)];
-  gamePattern.push(randomNumber + " " + randomChosenColor);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $(".btn").click(function() {
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+  });
+
+console.log(gamePattern);
 };
 
-console.log(nextSequence());
+nextSequence();
