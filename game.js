461fdcc4287bdac0fabc0cafc12cb67bbf803f87
Tiffany Valentine <52210944+devWitch-ctrl@users.noreply.github.com>
2020-08-13 15:54:32 -0600

    Third Commit
    Added event listeners to button clicks

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 
+let userClickedPattern = [];
 
 let gamePattern = [];
 
@@ -8,9 +9,13 @@ function nextSequence() {
 
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
+    gamePattern.push(randomChosenColor);
 
   $(".btn").click(function() {
+
+    let userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    console.log(userChosenColor);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
@@ -18,7 +23,11 @@ function nextSequence() {
     audio.play();
   });
 
-console.log(gamePattern);
+
 };
 
+  console.log(gamePattern);
+
 nextSequence();
+
+console.log(userClickedPattern);
