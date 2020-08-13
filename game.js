e8eb1a9718aff8f549e365ce517875d906e3e1c4
Tiffany Valentine <52210944+devWitch-ctrl@users.noreply.github.com>
2020-08-13 16:31:19 -0600

    Fourth Commit
    What I am doing even lol
    I mean, I'm experimenting with JQUERY to add sound function (play corresponding sound when button is clicked)

--- game.js
+++ game.js
@@ -4,30 +4,35 @@ let userClickedPattern = [];
 
 let gamePattern = [];
 
+//detect button click
+
+$(".btn").click(function() {
+  let userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  console.log(userChosenColor);
+});
+
+//generate random color/letter sequence
 
 function nextSequence() {
 
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
+  gamePattern.push(randomChosenColor);
 
-  $(".btn").click(function() {
-
-    let userChosenColor = this.id;
-    userClickedPattern.push(userChosenColor);
-    console.log(userChosenColor);
-
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-    audio.play();
-  });
-
-
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 };
-
-  console.log(gamePattern);
 
 nextSequence();
 
+console.log(gamePattern);
 console.log(userClickedPattern);
+
+//make sound which corresponds to button click
+
+function playSound(name) {
+  $('.btn').click(function() {
+    let audio = new Audio("sounds/" + userChosenColor + ".mp3");
+    audio.play();
+  });
+};
